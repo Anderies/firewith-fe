f0a0b226ce8d3c6f18d9c9379010da6cd1dee8ce
anderies <anderies@sprout.co.id>
2020-06-07 23:15:57 +0700

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,6 @@ import { FirewithTeamComponent } from './firewith-team/firewith-team.component';
 import * as Hammer from 'hammerjs';
 import { FirewithContactComponent } from './firewith-contact/firewith-contact.component';
 import { FooterComponent } from './footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-
 export class MyHammerConfig extends HammerGestureConfig {
   overrides = <any>{
     swipe: { direction: Hammer.DIRECTION_ALL },
@@ -31,8 +28,6 @@ export class MyHammerConfig extends HammerGestureConfig {
     BrowserModule,
     AppRoutingModule,
     HammerModule,
-    BrowserAnimationsModule,
-    MatButtonModule
   ],
   providers: [
     { provide: HAMMER_GESTURE_CONFIG, useClass: MyHammerConfig }
